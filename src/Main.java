14c1d5335fa537609c92cca736352461ce2b70bc
Ivan Diliso <diliso.ivan@gmail.com>
2019-10-24 00:01:34 +0200

    main cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 import index.*;
-import org.apache.pdfbox.text.PDFTextStripper;
 import ranking.Pair;
 import ranking.Query;
 import ranking.Retriever;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import utils.FileUtils;
-
-import java.io.File;
 import java.io.IOException;
 import java.util.Vector;
 
@@ -14,30 +9,11 @@ public class  Main {
 
     public static void main(String[] args) throws NotValidDocumentException, IOException {
 
-        FileUtils fu = new FileUtils();
-        Vector<File> mario = fu.getFolderFilesRecursive("/home/navis/workspace", "pdf");
-
-        for(File a : mario){
-            System.out.println(a.getName());
-        }
-
-        Document a = new Document("Mario", "/home/navis/workspace/cosine-similarity/documents/Astrazione_Progettazione.pdf");
-        DocumentProcesser processer = new DocumentProcesser();
-        WordBag bag = processer.generateWordBag(a);
-        System.out.println(bag);
-        ReverseIndex index = new ReverseIndex();
-        index.populateIndex(a);
-        System.out.println(index);
-
-
         Retriever ir = new Retriever();
-        //Document a = new Document("a", "...mario...ciao ....jaaja..exe.abc.pdf");
-        //System.out.println(a.getFileType());
-        ir.loadFolder("documents/");
+        ir.loadFolder("/home/navis/Uni");
 
-        Query query = new Query("funzionale");
+        Query query = new Query("diagramma di stati");
         Vector<Pair> lista = new Vector<>();
-
 
         for(Document d : ir.getDocuments()){
             lista.add(new Pair(d.getName(), (ir.cosineSimilarity(query, d))));
